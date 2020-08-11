5aec42fcc79cffbb958c18766ad3e6660c64e215
J lanpartis <jm@MacBook-Pro.local>
2020-08-11 11:51:44 +0800

    使用env_fn方式传递环境，便于将来分布式训练

--- tianshou/__init__.py
+++ tianshou/__init__.py
@@ -1,7 +1,10 @@
 from tianshou import data, env, utils, policy, trainer, \
     exploration
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> 使用env_fn方式传递环境，便于将来分布式训练
 __version__ = '0.2.6'
 __all__ = [
     'env',

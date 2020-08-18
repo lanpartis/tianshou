886601b51665654c4e8798c5584dbe45cf0a9d17
焦泳程 <jiaoyongcheng@digisky.com>
2020-08-18 11:55:48 +0800

    update version code

--- tianshou/__init__.py
+++ tianshou/__init__.py
@@ -1,11 +1,8 @@
 from tianshou import data, env, utils, policy, trainer, \
     exploration
 
-<<<<<<< HEAD
 
-=======
->>>>>>> 使用env_fn方式传递环境，便于将来分布式训练
-__version__ = '0.2.6'
+__version__ = '0.2.7'
 __all__ = [
     'env',
     'data',

9abb0955b890ee3b79417e26ea5e0c543a7d8e2d
douqiang <douqiang@digisky.com>
2020-09-18 12:02:59 +0800

    支持对result_pd续写功能

--- tianshou/trainer/offpolicy.py
+++ tianshou/trainer/offpolicy.py
@@ -33,7 +33,8 @@ def offpolicy_trainer(
         verbose: bool = True,
         test_in_train: bool = True,
         logger: Logger = None,
-        start_epoch: int = 1
+        start_epoch: int = 1,
+        result_df: pd.DataFrame = pd.DataFrame()
 ) -> Dict[str, Union[float, str]]:
     """A wrapper for off-policy trainer procedure. The ``step`` in trainer
     means a policy network update.
@@ -80,7 +81,6 @@ def offpolicy_trainer(
     global_step = start_epoch * collect_per_step
 
     best_epoch, best_reward = -1, -1.
-    result_df = pd.DataFrame()
     stat = {}
     start_time = time.time()
     test_in_train = test_in_train and train_collector.policy == policy

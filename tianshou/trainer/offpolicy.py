b71eaf997d98da76631dbadbe48d2cbdae5262a9
douqiang <douqiang@digisky.com>
2020-10-12 10:26:36 +0800

    添加start_epoch下限

--- tianshou/trainer/offpolicy.py
+++ tianshou/trainer/offpolicy.py
@@ -79,7 +79,7 @@ def offpolicy_trainer(
 
     :return: See :func:`~tianshou.trainer.gather_info`.
     """
-    global_step = (start_epoch-1) * step_per_epoch * collect_per_step
+    global_step = (max(start_epoch, 1) - 1) * step_per_epoch * collect_per_step
 
     best_epoch, best_reward = -1, -1.0
     stat: Dict[str, MovAvg] = {}
